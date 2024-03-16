50feb3455c7bd0300f1322b221e505e33ee97e6a
Aditya <adityaadiirai.004@gmail.com>
2024-03-16 15:07:48 +0530

    change in return body of login and register

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,9 +5,15 @@ const register = async (req, res, next) => {
     const { email, password } = req.body;
 
     try {
-        const user = new User({ email, password });
-        await user.save();
-        res.json({ message: 'registration successful' })
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: 'Email already exists' });
+        }
+
+        const newUser = new User({ email, password });
+        const savedUser = await newUser.save();
+
+        res.status(201).json({ success: true, message: 'Registration successful' });
     } catch (error) {
         next(error)
     }
@@ -18,19 +24,19 @@ const login = async (req, res, next) => {
     try {
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ success: false, message: 'User not found' });
         }
 
         const passwordMatch = await user.comparePassword(password);
 
         if (!passwordMatch) {
-            return res.status(401).json({ message: 'Incorrect password' });
+            return res.status(401).json({ success: false, message: 'Incorrect password' });
         }
 
         const token = jwt.sign({ userId: user._id }, 'secretkey', {
             expiresIn: '1 hour'
         });
-        res.json({ token });
+        res.json({ success: true, message: token });
     }
     catch (error) {
         next(error)
