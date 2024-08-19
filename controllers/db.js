689f9e873a93ff34a12a59d35b312379c4706ced
Aditya <adityaadiirai.004@gmail.com>
2024-08-19 20:31:46 +0530

    Moved DB pass to Env

--- controllers/db.js
+++ controllers/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://admin:Pass123456@trendingtimesapi.l72ul6k.mongodb.net/News-API?retryWrites=true&w=majority&appName=TrendingTimesAPI");
+    await mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@trendingtimesapi.l72ul6k.mongodb.net/News-API?retryWrites=true&w=majority&appName=TrendingTimesAPI`);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.log('Error connecting to MongoDB:', error);
